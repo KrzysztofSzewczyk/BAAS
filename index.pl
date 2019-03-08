eea50ff5e873ac3fbc0f30d5db1c1ffb6c7f8692
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-03-08 19:23:36 +0100

    Index stub

---
+++ index.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use CGI;
+
+my $request = CGI->new();
+print $request->header;
+
+my $html = <<'END';
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Online brainfuck interpreter.</h2>
+
+<form method="POST" action="/bfaas/brainfuck.pl">
+  <input type="text" name="code" value="+>+[#">
+  <br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>After clicking submit, brainfuck code will be executed on server.</p>
+<p>Limits: 256 cells of memory, 99 999 brainfuck instructions total.</p>
+
+</body>
+</html>
+END
+
+print $html
